3738fcf71605490211f5a8b25862886114a38216
Doctor邓 <Doctor@192.168.31.242>
2016-11-02 22:54:39 +0800

    监听器部分练习

--- src/listener/MyServletContextListener.java
+++ src/listener/MyServletContextListener.java
@@ -10,12 +10,14 @@ public class MyServletContextListener implements ServletContextListener {
 
 	@Override
 	public void contextDestroyed(ServletContextEvent event) {
-		
+		System.out.println("====监听器销毁====");
 	}
 
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
+		System.out.println("====监听器初始化====");
 		ServletContext sc = event.getServletContext();
+		//获取上下文初始化参数
 		String dogBreed = sc.getInitParameter("breed");
 		Dog d = new Dog(dogBreed);
 		sc.setAttribute("dog", d);
