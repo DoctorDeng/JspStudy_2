e7317287dda815f2da349e27bd36da3073b306ac
Doctor邓 <Doctor@192.168.31.242>
2016-11-03 23:44:22 +0800

    监听器练习

--- src/servlet/TestServlet.java
+++ src/servlet/TestServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import listener.User;
+
 /**
  * Servlet implementation class TestServlet
  */
@@ -25,15 +27,30 @@ public class TestServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String[] bankids = request.getParameterValues("bankIds");
+		//测试 Ajax 传递数组，后台是否可以接收
+		/*String[] bankids = request.getParameterValues("bankIds");
 		System.out.println("bankIds" + bankids);
 		for (int i = 0, len = bankids.length; i < len; i++) {
 			System.out.println("bankId:" + bankids[i]);
-		}
-		/*String[] boxs  = request.getParameterValues("testBox");
-		for (int i = 0, len = boxs.length; i < len; i++) {
-			System.out.println("boxs:" + boxs[i]);
 		}*/
+		//测试 对于属性的增加删除进行监听的监听器
+		/*request.setAttribute("id", bankids[0]);
+		request.getServletContext().setAttribute("id", bankids[0]);
+		request.getSession().setAttribute("id", bankids[0]);
+		
+		request.setAttribute("id", bankids[1]);
+		request.getServletContext().setAttribute("id", bankids[1]);
+		request.getSession().setAttribute("id", bankids[1]);
+		request.removeAttribute("id");
+		request.getServletContext().removeAttribute("id");
+		request.getSession().removeAttribute("id");*/
+		
+		//测试 监听绑定 到HttpSession 中某个对象的状态的时间监听器
+		User user = new User();
+		user.setPassowrd("123");
+		user.setUsername("DoctorDeng");
+		
+		request.getSession().setAttribute("currentUser", user);
 	}
 
 	/**
